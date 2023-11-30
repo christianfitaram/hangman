04430ef0ab565342c2057e27994b683474dfc7fd
christianfitaram <christianfitaram@gmail.com>
2023-11-30 19:49:17 +0100

    BUCLES WHEN LETTER GUESSED AND NOT GUESSED

--- hangman.java
+++ hangman.java
@@ -11,49 +11,75 @@ public class hangman {
         Scanner in = new Scanner(System.in);
         String palabra;
         int longitud;
-        int errores = 0;
-        String palabraNueva = "";
-        String registroInc = "";
-        String registroCor = "";
-        boolean control = true;
-        char letraIn;
-
-        System.out.println("Bienvenido al juego, introduce una palabra: ");
-        palabra = in.nextLine();
-        longitud = palabra.length();
-
-        while (control == true) {
-
-            System.out.println("Tienes " + (7 - errores) + " intentos");
-
+        boolean control = true, juego = true;
+        do {
+            System.out.println("Bienvenido al juego, introduce una palabra: ");
+            palabra = in.nextLine();
+            longitud = palabra.length();
+            control = true;
+            String palabraNueva = "";
+            int errores = 0;
+            String registroInc = "";
+            String registroCor = "";
+            char letraIn;
             for (int i = 1; i <= longitud; i++) {
                 palabraNueva += "-";
-
             }
+            while (control == true) {
 
-            System.out.println("Introduce una letra: ");
-            letraIn = in.nextLine().charAt(0);
+                System.out.println("Tienes " + (7 - errores) + " intentos");
 
-            if (palabra.indexOf(letraIn) > -1) {
-                registroCor = registroCor + letraIn;
+                System.out.println("Introduce una letra: ");
+                letraIn = in.nextLine().charAt(0);
 
-                for (int i = 0; i < longitud; i++) {
-                    if (palabra.charAt(i) == letraIn)
-                        palabraNueva = palabraNueva.substring(0, i) + letraIn
-                                + palabraNueva.substring((i + 1), (longitud));
+                if (palabra.indexOf(letraIn) > -1) {
+                    registroCor = registroCor + letraIn;
+
+                    for (int i = 0; i < longitud; i++) {
+                        if (palabra.charAt(i) == letraIn)
+                            palabraNueva = palabraNueva.substring(0, i) + letraIn
+                                    + palabraNueva.substring((i + 1), (longitud));
+                    }
+                    System.out.println(palabraNueva);
+                } else if (registroInc.indexOf(letraIn) > -1) {
+                    System.out.println("Ya has intentando con la letra " + letraIn + " prueba otra.");
+                } else {
+                    errores++;
+                    registroInc += letraIn;
+                    System.out.println("Letra equivocada.");
                 }
-                System.out.println(palabraNueva);
-            } else if (registroInc.indexOf(letraIn) > -1) {
-                System.out.println("Ya has intentando con la letra " + letraIn + " prueba otra.");
-            } else {
-                errores++;
-                registroInc += letraIn;
-                System.out.println(errores);
+                if (errores == 7) {
+                    System.out.println("Has agotado todos tus intentos");
+                    control = false;
+                    System.out.println("Deseas volver a jugar y/n");
+                    char respuesta = in.nextLine().charAt(0);
+                    if (respuesta == 'y' || respuesta == 'Y') {
+                        juego = true;
+                        
+                    } else if (respuesta == 'n' || respuesta == 'N') {
+                        System.out.println("Muchas gracias por jugar. Adios");
+                        juego = false;
+                        control = false;
+                    }
+
+                }
+                if (palabra.equals(palabraNueva)) {
+                    System.out.println("Has ganado");
+                    System.out.println("La palabra era: " + palabra);
+                    control = false;
+                    System.out.println("Deseas volver a jugar y/n");
+                    char respuesta = in.nextLine().charAt(0);
+                    if (respuesta == 'y' || respuesta == 'Y') {
+                        juego = true;
+
+                    } else if (respuesta == 'n' || respuesta == 'N') {
+                        System.out.println("Muchas gracias por jugar. Adios");
+                        juego = false;
+                        control = false;
+                    }
+                }
             }
-            if (errores == 7) {
-                control = false;
-            }
-        }
+        } while (juego == true);
 
     }
 }
